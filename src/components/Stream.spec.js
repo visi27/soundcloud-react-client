96b82f5c473e30cad0ece0b8dab04f56209166b4
Evis Bregu <evis.bregu@gmail.com>
2018-02-16 12:04:07 +0100

    Testing Stream component

---
+++ src/components/Stream.spec.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Stream from './function/Stream';
+import { shallow } from 'enzyme';
+
+describe('Stream', () => {
+
+  const props = {
+    tracks: [{ title: 'x' }, { title: 'y' }],
+  };
+
+  it('shows two elements', () => {
+    const element = shallow(<Stream { ...props } />);
+
+    expect(element.find('.track')).to.have.length(2);
+  });
+
+});
