defef8f4236256483c6df0967fd9f879e61532f8
Evis Bregu <evis.bregu@gmail.com>
2018-03-01 11:08:42 +0100

    Added redux DevTools

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { Provider } from 'react-redux'
 import configureStore from './stores/configureStore'
-import * as actions from './actions'
 import App from './components/App'
 import Callback from './components/Callback'
 import Stream from './components/Stream/index'
 import { CLIENT_ID, REDIRECT_URI } from './constants/auth'
+import DevTools from './containers/DevTools'
 
 SC.initialize({client_id: CLIENT_ID, redirect_uri: REDIRECT_URI})
 
@@ -20,6 +20,7 @@ const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <Provider store={store}>
+    <div>
     <Router history={history}>
       <Route path="/" component={App}>
         <IndexRoute component={Stream}/>
@@ -27,6 +28,8 @@ ReactDOM.render(
         <Route path="/callback" component={Callback}/>
       </Route>
     </Router>
+    <DevTools/>
+    </div>
   </Provider>,
   document.getElementById('app')
 )
--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,15 +1,31 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { createLogger } from 'redux-logger'
 import rootReducer from '../reducers/index'
 import { browserHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
+import DevTools from '../containers/DevTools';
 
 const logger = createLogger();
 const router = routerMiddleware(browserHistory);
 
-const createStoreWithMiddleware = applyMiddleware(thunk, router, logger)(createStore);
+const enhancer = compose(
+  // Middleware you want to use in development:
+  applyMiddleware(thunk, router, logger),
+  // Required! Enable Redux DevTools with the monitors you chose
+  DevTools.instrument()
+);
 
 export default function configureStore(initialState) {
-  return createStoreWithMiddleware(rootReducer, initialState);
+
+  const store = createStore(rootReducer, initialState, enhancer);
+
+  // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
+  if (module.hot) {
+    module.hot.accept('../reducers', () =>
+      store.replaceReducer(require('../reducers')/*.default if you use Babel 6+ */)
+    );
+  }
+
+  return store;
 }
